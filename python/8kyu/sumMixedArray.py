1c42f05c74a58126d9b5202f24a0a6353bc1a973
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2023-12-03 00:14:47 +0100

    python katas

---
+++ python/8kyu/sumMixedArray.py
@@ -0,0 +1,6 @@
+def sum_mix(arr):
+    sum = 0
+    for i in range(len(arr)):
+        sum += int(arr[i])
+    
+    return sum
---
+++ python/8kyu/theFeastOfManyBeast.py
@@ -0,0 +1,2 @@
+def feast(beast, dish):
+    return True if (beast[0] == dish[0] and beast[len(beast) - 1] == dish[len(dish) - 1]) else False
