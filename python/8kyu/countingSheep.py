abc3eb3527a14cdfcb8abcbc90eb12f3980c3352
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2023-12-16 22:21:55 +0100

    python 8kyu katas

---
+++ python/8kyu/countingSheep.py
@@ -0,0 +1,7 @@
+def count_sheeps(sheep):
+    sum = 0
+    for i in sheep:
+        if i is not None:
+            sum += i
+        
+    return sum
---
+++ python/8kyu/lostWithoutAMap.py
@@ -0,0 +1,5 @@
+def maps(a):
+    for i in range(0, len(a)):
+        a[i] = a[i] * 2
+        
+    return a
